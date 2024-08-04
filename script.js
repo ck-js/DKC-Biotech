02ee367ab147ead19d9d561717fa86ccbd3916b4
ck-js <ckentertainmentinfo@gmail.com>
2024-08-04 21:26:39 +0700

    take apart section 1 to remind myself why we wrote as we did and make sense if the sensing was sensing or not. and implement js for section 2 which will be our slideshow of images that we were going to amend on section 1 but decide not to and rather add new sections for new features rather than changing old sections with new features.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const mobileMenu = document.querySelector('#mobile-menu')
+mobileMenu.addEventListener('click', openCloseMobileMenu)
+
 function openCloseMobileMenu() {
   const navLinks = document.querySelector('.nav-links')
   
@@ -8,5 +11,25 @@ function openCloseMobileMenu() {
   navLinks.style.display = 'block'
   
 }
-const mobileMenu = document.querySelector('#mobile-menu')
-mobileMenu.addEventListener('click', openCloseMobileMenu)
+
+const bgSection1 = document.querySelector('.bg-section-1')
+const bg1 = './assets/rename/img4.webp';
+bgSection1.style.backgroundImage = `url(${bg1})` 
+// bgSection1.style.backgroundPosition = 'center';
+// bgSection1.style.backgroundSize = 'cover';
+// bgSection1.style.backgroundRepeat = 'no-repeat';
+
+
+function createSection4() {
+const parentContainer = document.createElement('div');
+
+const h1 = document.createElement('h1')
+h1.textContent = 'hello';
+
+parentContainer.appendChild(h1)
+
+return parentContainer;
+}
+const section4Element = createSection4();
+const section1Element = document.querySelector('.section-1-container')
+section1Element.insertAdjacentElement('afterend',section4Element)

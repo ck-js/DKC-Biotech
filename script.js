93b9a99ef79b7a5b7706e4524564fb8f93dd10fd
ck-js <ckentertainmentinfo@gmail.com>
2024-08-05 15:33:58 +0700

    implement carousel image feature and it scales with mobile / desktop width as we desire.

--- script.js
+++ script.js
@@ -20,16 +20,59 @@ bgSection1.style.backgroundImage = `url(${bg1})`
 // bgSection1.style.backgroundRepeat = 'no-repeat';
 
 
-function createSection4() {
-const parentContainer = document.createElement('div');
+// function createSection4() {
+// const parentContainer = document.createElement('div');
 
-const h1 = document.createElement('h1')
-h1.textContent = 'hello';
+// const h1 = document.createElement('h1')
+// h1.textContent = 'hello';
 
-parentContainer.appendChild(h1)
+// parentContainer.appendChild(h1)
 
-return parentContainer;
+// return parentContainer;
+// }
+// const section4Element = createSection4();
+// const section1Element = document.querySelector('.section-1-container')
+// section1Element.insertAdjacentElement('afterend',section4Element)
+
+
+// retreive each slide wrapper element
+const slideElements = document.querySelectorAll('.slide-wrappers')
+const slideElementsArray = Array.from(slideElements);
+const firstSlide = slideElementsArray[0];
+const secondSlide = slideElementsArray[1];
+const thirdSlide = slideElementsArray[2];
+
+
+// firstSlide.classList.remove('current-slide')
+// secondSlide.classList.add('current-slide')
+
+function nextSlide() {
+  const currentSlide = document.querySelector('.current-slide')
+  // const currentSlideCircle = document.querySelector('.current-slide-circle')
+
+// alert(currentSlide.id)
+if (currentSlide.id === 'slide-1') {
+  currentSlide.classList.remove('current-slide')
+  secondSlide.classList.add('current-slide')
+
+//   currentSlideCircle.classList.remove('current-slide-circle')
+// slideCirclesArray[1].classList.add('current-slide-circle')
 }
-const section4Element = createSection4();
-const section1Element = document.querySelector('.section-1-container')
-section1Element.insertAdjacentElement('afterend',section4Element)
+if (currentSlide.id === 'slide-2') {
+  currentSlide.classList.remove('current-slide')
+  thirdSlide.classList.add('current-slide')
+
+//   currentSlideCircle.classList.remove('current-slide-circle')
+// slideCirclesArray[2].classList.add('current-slide-circle')
+}
+if (currentSlide.id === 'slide-3') {
+  currentSlide.classList.remove('current-slide')
+  firstSlide.classList.add('current-slide')
+
+//   currentSlideCircle.classList.remove('current-slide-circle')
+// slideCirclesArray[0].classList.add('current-slide-circle')
+}
+
+}
+setInterval(nextSlide,2000)
+
